ead769c2709c0bfa162e2992c4609bb1f55ccd76
HA JIN <w2128@e-mirim.hs.kr>
2023-08-11 10:19:01 +0900

    header 생성

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/header.css";
+
+const Header = () => {
+    return(
+        <div className="container">
+            <h1>My Voca</h1>
+            <div className="nav-box">
+                <div className="nav-tab">단어</div>
+                <div className="nav-tab">예문</div>
+                <div className="nav-tab">마이페이지</div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
