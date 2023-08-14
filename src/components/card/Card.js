5eda3123e9c4ba876fc09bc9656cfbd544d6f993
HA JIN <w2128@e-mirim.hs.kr>
2023-08-15 02:21:34 +0900

    북마크, 암기완료 버튼 클릭 시 값 변경, 변경된 값을 부모 컴포넌트에게 전달

---
+++ src/components/card/Card.js
@@ -0,0 +1,52 @@
+import off from "../../assets/off.png";
+import on from "../../assets/on.png";
+import check from "../../assets/check.png";
+import circle from "../../assets/circle.png";
+import "./card.css";
+// import { useEffect } from "react";
+import { useState } from "react";
+
+const Card = ({wordCard, onWordCardChange, onBookmarkChange}) => {
+  const { word, mean, isChecked, isBookmarked } = wordCard;
+
+  const [checked, setChecked] = useState(isChecked); //암기완료
+  const [bookmark, setBookmark] = useState(isBookmarked); //즐겨찾기
+
+  const handleChange = (button) => {
+    let updatedWordCard;
+    if(button === "check"){
+      setChecked(checked => !checked);
+      updatedWordCard = {...wordCard, isChecked: checked};
+    } else {
+      setBookmark(!bookmark);
+      updatedWordCard = {...wordCard, isBookmarked: bookmark};
+    }
+    onWordCardChange(updatedWordCard);
+  };
+  //값 변경 테스트
+  // useEffect(()=>{
+  //   console.log('암기완료 했는가? ->', checked)
+  //   // console.log('즐겨찾기 했는가? ->', bookmark);
+  // }, [checked])
+  return (
+    <div className="card-container">
+      <img className="bookmarkImg" 
+          src={bookmark ? on : off} 
+          alt="즐겨찾기"
+          onClick={()=>{handleChange("bookmark")}}/>
+      <div>
+        <div className="word">{word}</div>
+        <div className="mean">{mean}</div>
+      </div>
+      <div className="checkBox">
+        <div className="checkText">DONE</div>
+        <img className="checkImg" 
+            src={checked ? check : circle} 
+            onClick={()=>{handleChange("check")}}
+            alt="체크"/>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
