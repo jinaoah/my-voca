abb180fa83a5b608d547ac958e1213ac3031723f
HA JIN <w2128@e-mirim.hs.kr>
2023-08-15 02:22:34 +0900

    홈페이지 코드 간결화

---
+++ src/pages/homepage/home.js
@@ -0,0 +1,16 @@
+import Tab from "../../components/tab/Tab";
+import SearchBox from "../../components/searchBox/searchBox";
+import "./home.css";
+
+const Home = () => {
+
+  return (
+    <div>
+      <SearchBox />
+      <Tab />
+      
+    </div>
+  );
+};
+
+export default Home;
