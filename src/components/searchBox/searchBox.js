778eafa49757aafd8e4f810807e617c6465cace4
HA JIN <w2128@e-mirim.hs.kr>
2023-08-15 02:25:56 +0900

    react-router-dom 설치, 카드 추가 페이지 이동

--- src/components/searchBox/searchBox.js
+++ src/components/searchBox/searchBox.js
@@ -1,13 +1,19 @@
 import "./searchBox.css";
+import { useNavigate } from "react-router-dom";
 
-const searchBox = () => {
+const SearchBox = () => {
+  const navigate = useNavigate();
+
+  const goToAddCard = () => {
+    navigate("add-card");
+  }
   return (
     <div className="box">
       <input className="input" placeholder="검색창" />
-      <button className="add">+</button>
+      <button className="add" onClick={goToAddCard}> +</button>
       <button className="delete">삭제</button>
     </div>  
   );
 };
 
-export default searchBox;
+export default SearchBox;
