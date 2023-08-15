63e7f0cfa117783cf3a30919f3eb15fb8d53c271
HA JIN <w2128@e-mirim.hs.kr>
2023-08-16 07:45:04 +0900

    헤더에게 useNavigate를 props로 넘김 (부모->자식)

--- src/pages/homepage/home.js
+++ src/pages/homepage/home.js
@@ -1,11 +1,14 @@
 import Tab from "../../components/tab/Tab";
 import SearchBox from "../../components/searchBox/searchBox";
 import "./home.css";
+import Header from "../../components/header/Header";
+import {useNavigate } from "react-router-dom";
 
 const Home = () => {
-
+  const navigate = useNavigate();
   return (
     <div>
+      <Header navigate={navigate}/>
       <SearchBox />
       <Tab />
       
