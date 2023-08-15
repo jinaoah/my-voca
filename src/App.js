b4077e60c7b339ca774de239ecd8cfadac76e4bf
HA JIN <w2128@e-mirim.hs.kr>
2023-08-16 07:44:06 +0900

    마이페이지 라우트 연결

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Header from './components/header/Header';
 import Home from './pages/homepage/home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddCard from './pages/addcard/AddCard';
+import Mypage from './pages/mypage/mypage';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
+      {/* <Header navigate={navigate} /> */}
+      {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/add-card" element={<AddCard />}/>
+          <Route path="/mypage" element={<Mypage />} />
         </Routes>
       </BrowserRouter>
     </div>
