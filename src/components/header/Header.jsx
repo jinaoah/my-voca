12490dc0abf3e274e31ad03ad9257dbd3ad7e23f
HA JIN <w2128@e-mirim.hs.kr>
2023-08-16 07:44:34 +0900

    헤더 메뉴 이동

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./header.css";
+import {useLocation} from "react-router-dom";
 
-const Header = () => {
+const Header = ({navigate}) => {
+    const location = useLocation();
     return(
         <div className="container">
-            <h1>My Voca</h1>
+            <h1 onClick={()=>navigate("/")}>My Voca</h1>
             <div className="nav-box">
-                <div className="nav-tab">단어</div>
-                <div className="nav-tab">예문</div>
-                <div className="nav-tab">마이페이지</div>
+                <div className={`nav-tab ${location.pathname === "/"?"active":""}`} onClick={()=>{navigate("/")}}>단어</div>
+                <div className={`nav-tab ${location.pathname === "/sentence"?"active":""}`} onClick={()=>{navigate("/sentence")}}>예문</div>
+                <div className={`nav-tab ${location.pathname === "/mypage"?"active":""}`} onClick={()=>{navigate("/mypage")}}>마이페이지</div>
             </div>
         </div>
     );
