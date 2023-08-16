5c5696b3da115a645a4efb4c83ab1228720cab08
HA JIN <w2128@e-mirim.hs.kr>
2023-08-17 02:54:13 +0900

    js에서 제공하는 tts 서비스 함수

---
+++ src/pages/detailCard/speak.js
@@ -0,0 +1,21 @@
+
+export function speak(text, opt_prop) {
+    console.log('speack 함수')
+    if (typeof SpeechSynthesisUtterance === "undefined" || typeof window.speechSynthesis === "undefined") {
+        alert("이 브라우저는 음성 합성을 지원하지 않습니다.")
+        return
+    }
+    
+    window.speechSynthesis.cancel() // 현재 읽고있다면 초기화
+
+    const prop = opt_prop || {}
+
+    const speechMsg = new SpeechSynthesisUtterance()
+    speechMsg.rate = prop.rate || 0.5 // 속도: 0.1 ~ 10      
+    speechMsg.pitch = prop.pitch || 1 // 음높이: 0 ~ 2
+    speechMsg.lang = prop.lang || "ko-KR"
+    speechMsg.text = text
+    
+    // SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
+    window.speechSynthesis.speak(speechMsg)
+}
